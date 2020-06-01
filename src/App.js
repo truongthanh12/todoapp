1d714141aa636050930f403f15cd05d260b55362
truongthanh12 <48469016+truongthanh12@users.noreply.github.com>
2020-06-01 23:18:06 +0700

    d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Note from './Note'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      text: '',
+      notes: [],
+    }
+  }
 
+  updateText(text) {
+    this.setState({
+      text: text.target.value
+    })
+  }
+  addNote() {
+
+    if (this.state.text === '') { return }
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.text);
+    this.setState({ text: '' });
+    this.txtInput.focus();
+
+  }
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.text);
+      this.setState({ text: '' });
+    }
+  }
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr })
+  }
+  render() {
+    let notes = this.state.notes.map((val, key) => {
+      return <Note key={key} text={val} deleteMethod={() => this.deleteNote(key)}></Note>
+    })
+    return (
+      <div className="container">
+        <div className="header"> Todo Application </div>
+        <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+        <input type="text" ref={(input) => { this.txtInput = input }}
+          className="textInput" value={this.state.text} placeholder="Enter the text"
+          onChange={text => this.updateText(text)}
+          onKeyPress={this.handleKeyPress.bind(this)}>
+        </input>
+        {notes}
+
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/Note.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Note extends Component {
+    render() {
+        return (
+            <div className="note" onClick={this.props.deleteMethod}>
+                {this.props.text}
+            </div>
+        )
+    }
+}
